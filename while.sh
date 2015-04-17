2c640763caee40efe8ce16d4e9cd68f987a51c88
Giuseppe <I am Amerigo, you know my mail>
2015-04-17 11:26:38 +0200

    while skeleton

--- while.sh
+++ while.sh
@@ -1,10 +1,4 @@
-#! /bin/bash
-
-COUNT=10
-while (( COUNT > 0 ))
+while (( COUNT >0 ))
 do
-    echo -e "$COUNT \c" #This is telling "all in one line"
-    #sleep 1
     (( COUNT -- ))
 done
-echo -e "\n\nFIRE!!"
