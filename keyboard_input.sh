51717586f17396ed1ce54bbc0f93e6f0f21a0564
Giuseppe <I am Amerigo, you know my mail>
2015-04-17 11:45:31 +0200

    keyboard input skeleton

--- keyboard_input.sh
+++ keyboard_input.sh
@@ -1,13 +1,7 @@
-#! /bin/bash
-while true
-do
-    echo -e "What do you want me to take from keyborad?: \c"
-    read input
-    if [[ $input = q ]]
-    then
-        #exit #This closes the terminal
-	echo "Done"
-	break
-    fi
-done
+echo -e "What do you want me to take from keyborad?: \c"
+read input
+if [[ $input = q ]]
+then
+
+fi
 
