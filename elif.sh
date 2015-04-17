e6aa4f8d3c3abc2a0b30afdccbe97f227248cef5
Giuseppe <I am Amerigo, you know my mail>
2015-04-17 11:23:53 +0200

    elif skeleton

--- elif.sh
+++ elif.sh
@@ -1,13 +1,9 @@
-#! /bin/bash -x
-
-if [[ $1 = "ciao" ]] # This is comparison between string
+if [[ $1 = "ciao" ]] #comparison between string
 then 
-    echo "ciao anche a te"
-    find /etc -maxdepth 1 -type d 
-elif (( $1 <2 )) #This is comparison between numbers
-then 
-    echo "you said 1. I say two"
-else 
-    echo "The usage is: "$0 "ciao | 1"
 
+elif (( $1 <2 )) #comparison between numbers
+then
+ 
+else
+ 
 fi
