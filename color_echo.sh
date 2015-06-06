6f6eed8e5b2759dea2470a286944cdcf8e204542
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-06-06 17:01:48 +0200

    Update color_echo.sh

--- color_echo.sh
+++ color_echo.sh
@@ -1,3 +1,9 @@
 echo -e "\e[0;31m error \e[0m " #RED
 echo -e "\e[0;33m warning \e[0m "#YELLOW
 echo -e "\e[0;36m info \e[0m "#BLUE
+
+
+
+#0 is for dark colors, 1 is for light colors
+#foreground colors 0;30-37 or 1;40-47
+#background colors 0;40-47 or 1;30-37
