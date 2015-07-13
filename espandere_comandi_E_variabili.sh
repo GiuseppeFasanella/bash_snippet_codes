ecd935f4e2ad8ad386f88e435ac029a1f8a8921a
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-07-13 17:06:53 +0200

    Update espandere_comandi_E_variabili.sh

--- espandere_comandi_E_variabili.sh
+++ espandere_comandi_E_variabili.sh
@@ -1,5 +1,8 @@
 #Per espandere un comando usa la parentesi tonda $(head bla bla)
 #Per espandere una variabile o $file_to_play o meglio la parentesi graffa ${file_to_play}
 
+###NOTA BENE: quando assegni file_to_play NON lasciare spazio tra =$ perche' altrimenti non funziona!!
+###Madonna che sintassi pesante bash!
+
 file_to_play=$(head -1 list.dat);
 echo "file to play is" ${file_to_play};
