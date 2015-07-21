360d9f33a284b07210b93691c1059d46ef333151
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-07-21 15:52:28 +0200

    Update for.sh

--- for.sh
+++ for.sh
@@ -2,3 +2,11 @@ for F in $(ls) #for F in the output of ls
 do
 done
 
+#seq crea una sequenza con primo e ultimo elemento compresi
+for i in `seq 1 3`; do echo ${i}; done
+1
+2
+3
+
+# In generale, se vai a capo non c'e' bisogno di usare ;
+# Se vuoi fare tutto su una riga devi usare i ;
