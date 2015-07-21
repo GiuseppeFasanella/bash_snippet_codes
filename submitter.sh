22a64d42fae2e7bdfad0e3731261593f4de57d0b
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-07-21 21:00:54 +0200

    Update submitter.sh

--- submitter.sh
+++ submitter.sh
@@ -3,3 +3,5 @@ bsub -q 2nd -oo stdout.log -eo stderr.log -J "nome job"
 eval \`scramv1 runtime -sh\`; uname -a;  echo \$CMSSW_VERSION; comandone > debug.txt"
 
 #secondo me funziona anche con cd $PWD
+#stdout.log contiene il comando che viene processato, cosi' puoi controllare cosa ha fatto
+
