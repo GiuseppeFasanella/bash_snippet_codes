b938b91d77ff29d6195ac222d97fd1a5ffd491b8
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-10-27 09:17:06 +0100

    Create cartella_remota_montare_smontare.sh

---
+++ cartella_remota_montare_smontare.sh
@@ -0,0 +1,7 @@
+remote_dir_mount
+
+sshfs gfasanel@m6.iihe.ac.be:/user/gfasanel/HEEP/CMSSW_7_2_0_patch1/src/Mass_resolution/roofit/fit_results Scrivania/remote_dir
+
+(per smontare) sudo umount /home/gfasanel/Scrivania/remote_dir
+
+remote_dir_umount
