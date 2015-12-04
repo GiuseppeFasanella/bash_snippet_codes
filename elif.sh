d503d22396b6628e56390e3f31fd54a42f0e96de
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-12-04 10:40:51 +0100

    Update elif.sh

--- elif.sh
+++ elif.sh
@@ -5,10 +5,15 @@ first instruction #No needs for ; here
 second instruction 
 elif (( $1 <2 )) #comparison between numbers
 then
- 
 else
- 
 fi
+
+#se vuoi paragonare numeri interi DEVI fare cosi', usando eq
+if [ "$1" -eq "2" ] ||  [ "$1" -eq "3" ]
+then
+    echo "ciao"
+fi
+
 
 ## Se invece vuoi scrivere tutto su una riga sola, vanno usati i ;
 if [[ $1 = "ciao" ]];then echo "ciao";fi
