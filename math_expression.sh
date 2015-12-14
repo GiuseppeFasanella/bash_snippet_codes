7638ac9cf5683463ae1f3b79ec0f1ccad0b581f9
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-12-14 19:27:10 +0100

    Update math_expression.sh

--- math_expression.sh
+++ math_expression.sh
@@ -1,4 +1,10 @@
-$((expression))
+#bash supporta solo operazioni con numeri interi!!!!!
+n1=10
+n2=3
 $(( n1+n2 ))
 $(( n1/n2 ))
 $(( n1-n2 ))
+
+#se vuoi operare con floating point, devi ridirezionarlo a un programma capace che 
+echo "2.5+3.75" | bc -l
+# -l e' la libreria matematica
