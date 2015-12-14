bc697f86d28ce6e752f46ca5d9e7a11ff543dc9f
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-12-14 19:30:21 +0100

    Update math_expression.sh

--- math_expression.sh
+++ math_expression.sh
@@ -8,3 +8,5 @@ $(( n1-n2 ))
 #se vuoi operare con floating point, devi ridirezionarlo a un programma capace che 
 echo "2.5+3.75" | bc -l
 # -l e' la libreria matematica
+Per limitare la precisione di echo, puoi fare cosi':
+printf "%.3f\n" $variable
