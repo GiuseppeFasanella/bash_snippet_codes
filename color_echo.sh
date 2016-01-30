21f14c828220827fdcf08ccb7a55ca6f535fb98b
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2016-01-30 16:11:59 +0100

    Update color_echo.sh

--- color_echo.sh
+++ color_echo.sh
@@ -1,6 +1,6 @@
 echo -e "\e[0;31m error \e[0m " #RED
-echo -e "\e[0;33m warning \e[0m "#YELLOW
-echo -e "\e[0;36m info \e[0m "#BLUE
+echo -e "\e[0;33m warning \e[0m " #YELLOW
+echo -e "\e[0;36m info \e[0m " #BLUE
 
 
 
