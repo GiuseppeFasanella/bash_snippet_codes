83374dbb9af98f31763a991ab2c06affd4cd2629
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2016-06-03 12:08:42 +0200

    Update array.sh

--- array.sh
+++ array.sh
@@ -1,9 +1,9 @@
 #il pushback si fa cosi'
-ARRAY=()
-ARRAY+=('foo')
-ARRAY+=('bar')
+my_array=()
+my_array+=('foo')
+my_array+=('bar')
 
-for i in "${arrayName[@]}"
+for i in "${my_array[@]}"
 do
    :
    # do whatever on $i
@@ -12,8 +12,8 @@ done
 # qui $i e' un elemento di arrayName
 
 ##Se vuoi loopare sopra l'indice si fa cosi':
-for i in "${!foo[@]}"; do 
-  printf "%s\t%s\n" "$i" "${foo[$i]}"
+for i in "${!my_array[@]}"; do 
+  printf "%s\t%s\n" "$i" "${my_array[$i]}"
 done
 
 
