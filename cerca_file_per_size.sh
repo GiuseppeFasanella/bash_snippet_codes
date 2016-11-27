7062e3399582d3323517ffde7f154d1b61991479
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2016-11-27 22:54:24 +0100

    Update cerca_file_per_size.sh

--- cerca_file_per_size.sh
+++ cerca_file_per_size.sh
@@ -1,1 +1,13 @@
+```
 sudo find / -name '*' -size +500M
+```
+```
+find / -name 'program.c' 2>/dev/null
+Note : 2>/dev/null is not related to find tool as such. 2 indicates the error stream in Linux, 
+and /dev/null is the device where anything you send simply disappears. So 2>/dev/null 
+in this case means that while finding for the files, 
+in case any error messages pop up simply send them to /dev/null i.e. simply discard all error messages.
+```
+```
+find / -name 'program.c' 2>errors.txt
+```
