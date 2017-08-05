8c88aea06f0beed855a5ab7ef4902516f00f7627
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2017-08-05 23:22:02 +0200

    Create expand_variables_in_single_quotes.sh

---
+++ expand_variables_in_single_quotes.sh
@@ -0,0 +1,9 @@
+Inside single quotes everything is preserved literally, without exception.
+
+That means you have to close the quotes, insert something, and then re-enter again.
+
+'before'"$variable"'after'
+
+'before'\''after'
+
+'before'"'"'after'
