0b8605dd21c5c1d3c6184c363b68a5c9146faf16
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2018-01-16 15:48:21 +0100

    Update kill_process.sh

--- kill_process.sh
+++ kill_process.sh
@@ -1,3 +1,9 @@
 ps aux | grep "run_alarm.py"
 #La seconda colonna e' il process id (pid)
 sudo kill -9 pid
+
+###### Piu' rifinito
+
+ps -ef | grep BDT | grep gfasane | grep cross | awk '{print$2}'
+kill -9 $(ps -ef | grep BDT | grep gfasane | grep cross | awk '{print$2}')
+
