1056594527d8582ac5489debcb0b0c33fd030500
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2018-03-29 09:52:27 +0200

    Create check_if_argument_passed.sh

---
+++ check_if_argument_passed.sh
@@ -0,0 +1,7 @@
+#!/bin/bash                                                                                                                                                                             
+
+if [ -z "$1" ]
+then
+    echo  '========> Model Type D or E is mandatory'
+    exit 0
+fi
