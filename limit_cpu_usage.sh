8403a104b2357bdb04c3e1dac22be2480877efc0
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2020-01-09 20:48:06 +0100

    Create limit_cpu_usage.sh

---
+++ limit_cpu_usage.sh
@@ -0,0 +1,3 @@
+## 50% della cpu è il massimo utilizzo consentito
+cpulimit -l 50 -- python main.py --oper train --look_back 150 --look_ahead 45
+(base) gfasanel@Amerigo:~/stock_tools$ Process 15344 detected
