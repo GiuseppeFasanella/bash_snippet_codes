ee02ca2d27193149ed3420781215a11b57890909
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2020-03-04 11:27:31 +0100

    Update kill_process.sh

--- kill_process.sh
+++ kill_process.sh
@@ -1,3 +1,11 @@
+## Simple and clean
+pgrep -af "string_to_search_for"
+44983 python main.py --model model_da_bel_v1_15.py --oper build --load_start_dt 2016-01-01 --load_end_dt 2019-01-01 --reset_type model
+kill -9 44983
+
+
+## Other less clean versions
+###############################################
 ps aux | grep "run_alarm.py"
 #La seconda colonna e' il process id (pid)
 sudo kill -9 pid
